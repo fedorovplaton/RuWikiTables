b01d1daf7f526c81659de385158a84e93cb4afc6
Alexey Mironov <alexmir2015@yandex.ru>
2021-09-06 17:52:08 +0300

    context recognition to html page parser

--- src/model/PagesCrawler.py
+++ src/model/PagesCrawler.py
@@ -3,6 +3,28 @@ import pandas as pd
 
 from src.types.TableInfo import TableInfo
 
+stop_tags = ["h2"]
+
+
+def find_context_after(words_count: int, html_table):
+    result = []
+    after = html_table.find_next_sibling()
+    while after and after.name not in stop_tags and len(result) < words_count:
+        result.extend(after.text.split())
+        after = after.find_next_sibling()
+
+    return ' '.join(result[:words_count])
+
+
+def find_context_previous(words_count: int, html_table):
+    result = []
+    previous = html_table.findPreviousSibling()
+    while previous and previous.name not in stop_tags and len(result) < words_count:
+        result = previous.text.split() + result
+        previous = previous.findPreviousSibling()
+
+    return ' '.join(result[:words_count])
+
 
 def parse(html_text: str) -> any:
     """
@@ -20,12 +42,16 @@ def parse(html_text: str) -> any:
             after_context = ""
             title = ""
             try:
+                previous_context = find_context_previous(100, html_table)
+                after_context = find_context_after(100, html_table)
                 caption = html_table.find("caption")
                 if not caption is None:
                     title = html_table.find("caption").text
-                elif html_table.findPreviousSibling() and not html_table.findPreviousSibling().find('span', {'class': "mw-headline"}) is None:
+                elif html_table.findPreviousSibling() and not html_table.findPreviousSibling().find('span', {
+                    'class': "mw-headline"}) is None:
                     title = html_table.findPreviousSibling().find('span', {'class': "mw-headline"}).text
-                elif html_table.findPrevious("h2") and not html_table.findPrevious("h2").find('span', {'class': "mw-headline"}) is None:
+                elif html_table.findPrevious("h2") and not html_table.findPrevious("h2").find('span', {
+                    'class': "mw-headline"}) is None:
                     title = html_table.findPrevious("h2").find('span', {'class': "mw-headline"}).text
                 tables.append(TableInfo(previous_context, after_context, table, title))
             except Exception as error:
